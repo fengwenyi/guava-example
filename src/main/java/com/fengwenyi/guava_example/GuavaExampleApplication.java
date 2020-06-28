c66a0432c6ab5d4ce4a9e797ceb75db6b3ad4165
冯文议 <xfsy_2015@163.com>
2020-06-28 14:33:46 +0800

    init

---
+++ src/main/java/com/fengwenyi/guava_example/GuavaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.fengwenyi.guava_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuavaExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuavaExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fengwenyi/guava_example/GuavaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fengwenyi.guava_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuavaExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
